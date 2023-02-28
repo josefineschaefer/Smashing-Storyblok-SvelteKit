687faa5c2c855ebb37c5217092702e31965b352b
josefineschaefer <josefine.schaefer@storyblok.com>
2023-02-28 11:36:26 +0100

    initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let header;
+</script>
+
+<header class="w-full h-24 bg-[#f7f6fd]">
+	<div class="container h-full mx-auto flex items-center justify-between">
+		<a href="home">
+			<h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok SvelteKit</h1>
+		</a>
+		{#if header}
+			<nav>
+				<ul class="flex space-x-8 text-lg font-bold">
+					{#each header as blok}
+						<li class="hover:text-[#50b0ae]">
+							<a href={blok.link.story.slug}> {blok.link.story.name}</a>
+						</li>
+					{/each}
+				</ul>
+			</nav>
+		{/if}
+	</div>
+</header>
